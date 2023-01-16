0a7b05f74249b68508cea36c89775a5e686c803e
+ <+>
2023-01-15 20:24:20 -0500

    commit tree ec290beefc2ffd3a336dea81c734a325b76b6f19

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,43 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Dancer
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+# class Dancer
+#     include Dance
+
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+# extend MetaDancing
+
+# extend FancyDance::ClassMethods
+#   include FancyDance::InstanceMethods
+# end
+
+# with non-namespaced modules
+
+
+
+
+# with namespaced modules
+# require_relative './fancy_dance'
+# class Dancer
+#   include FancyDance::InstanceMethods
+#   extend FancyDance::ClassMethods
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+# end
